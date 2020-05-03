e76647d6f52c814cc5aa406ea470957ee3683f40
chenxudong2020 <61214170+chenxudong2020@users.noreply.github.com>
2020-05-03 13:31:33 +0800

    fix

--- diy.sh
+++ diy.sh
@@ -7,13 +7,14 @@
 #=================================================
 
 device_name='HC5962'
+wifi_name='HiWiFi'
 lan_ip='192.168.199.1'        # Lan Ip地址
 utc_name='Asia\/Shanghai'   # 时区
 delete_bootstrap=true       # 是否删除默认主题 true 、false
 default_theme='argon_mc1'   # 默认主题 结合主题文件夹名字 
 theme_argon='https://github.com/sypopo/luci-theme-argon-mc.git'  # 主题地址
 openClash_url='https://github.com/vernesong/OpenClash.git'       # OpenClash包地址 
-
+lienol_url='https://github.com/Lienol/openwrt-package.git'       # Lienol 包地址
 adguardhome_url='https://github.com/rufengsuixing/luci-app-adguardhome.git' # adguardhome 包地址
 
  
@@ -30,6 +31,14 @@ sed -i "s/192.168.1.1/$lan_ip/g" package/base-files/files/bin/config_generate
 #修改默认时区
 echo "修改时区"
 sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='$utc_name'/g" package/base-files/files/bin/config_generate
+
+
+echo "修改wifi名称"
+
+sed -i "s/OpenWrt/$wifi_name/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+
+
 
 
 echo "修改默认主题"
@@ -82,5 +91,52 @@ echo 'CONFIG_PACKAGE_luci-i18n-openclash-zh-cn=y'  >> .config
 
 
 
+
+
+echo '添加Passwall'
+
+echo 'CONFIG_PACKAGE_luci-app-passwall=y' >> .config
+
+echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Shadowsocks=y' >> .config
+
+echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Trojan=y' >> .config
+
+echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_simple-obfs=y' >> .config
+
+echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_v2ray-plugin=y' >> .config
+
+echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Brook=y' >> .config
+
+echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_kcptun=y' >> .config
+
+echo 'CONFIG_PACKAGE_luci-i18n-passwall-zh-cn=y'  >> .config
+
+
+
+echo '添加主题'
+echo 'CONFIG_PACKAGE_luci-theme-netgear=y'  >> .config
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Define My Package
-git clone https://github.com/fw876/helloworld.git package/lean/luci-app-ssr-plus
+#git clone https://github.com/fw876/helloworld.git package/lean/luci-app-ssr-plus
