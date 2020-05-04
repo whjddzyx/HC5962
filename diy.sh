1890f10a79eb11f72ccc526e2801beb54ed09a45
chenxudong2020 <61214170+chenxudong2020@users.noreply.github.com>
2020-05-04 18:27:11 +0800

    修改默认主题1

--- diy.sh
+++ diy.sh
@@ -65,9 +65,10 @@ echo 'CONFIG_PACKAGE_luci-theme-argon-mc=y' >> .config
 
 
 echo "修改默认主题"
-#sed -i "s/bootstrap_mod/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
+#修改主题方法1
+sed -i "s/bootstrap_mod/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
 
-sed -i "s/bootstrap_mod/$default_theme/g" package/default-settings/files/zzz-default-settings
+#sed -i "s/bootstrap_mod/$default_theme/g" package/default-settings/files/zzz-default-settings
 
 
 
