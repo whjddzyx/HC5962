a00cc8c3a3323b94a39d89b5c00279b59dcbb7ee
chenxudong2020 <872603935@qq.com>
2020-06-24 13:46:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,5 +8,28 @@
 # Blog: https://p3terx.com
 #============================================================
 
+device_name='HC5962'
+wifi_name='HiWiFi'
+lan_ip='192.168.199.1'        # Lan Ip地址
+utc_name='Asia\/Shanghai'   # 时区
+
+
+ 
+#修改机器名称
+echo "设置主机名"
+sed -i "s/OpenWrt/$device_name/g" package/base-files/files/bin/config_generate
+
+
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+echo "设置lan ip"
+sed -i "s/192.168.1.1/$lan_ip/g" package/base-files/files/bin/config_generate
+
+
+#修改默认时区
+echo "修改时区"
+sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='$utc_name'/g" package/base-files/files/bin/config_generate
+
+
+echo "修改wifi名称"
+
+sed -i "s/OpenWrt/$wifi_name/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
