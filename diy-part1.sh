1e328df755ae281f7f6bd6ee27849a660c3ed4b6
chenxudong2020 <872603935@qq.com>
2020-07-08 14:20:58 +0800

    添加passWall

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,9 @@
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
 # Define My Package
 #git clone https://github.com/fw876/helloworld.git                                   package/molun/luci-app-ssr-plus
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome.git                 package/molun/luci-app-adguardhome
--- diy-part2.sh
+++ diy-part2.sh
@@ -39,3 +39,22 @@ sed -i "s/OpenWrt/$wifi_name/g" package/kernel/mac80211/files/lib/wifi/mac80211.
 #git clone -master $openClash_url package/lean/luci-app-openclash 
 #echo 'CONFIG_PACKAGE_luci-app-openclash=y' >> .config
 #echo 'CONFIG_PACKAGE_luci-i18n-openclash-zh-cn=y'  >> .config
+
+echo '添加Passwall'
+
+echo 'CONFIG_PACKAGE_luci-app-passwall=y' >> .config
+
+echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Shadowsocks=y' >> .config
+
+echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Trojan=y' >> .config
+
+echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_simple-obfs=y' >> .config
+
+echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_v2ray-plugin=y' >> .config
+
+echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Brook=y' >> .config
+
+echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_kcptun=y' >> .config
+
+echo 'CONFIG_PACKAGE_luci-i18n-passwall-zh-cn=y'  >> .config
+ 
