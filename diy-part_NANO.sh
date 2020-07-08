cc7c362ea936790e9ca11652c762762d7e72064c
chenxudong2020 <872603935@qq.com>
2020-07-08 18:30:41 +0800

    Update diy-part_NANO.sh

--- diy-part_NANO.sh
+++ diy-part_NANO.sh
@@ -35,6 +35,3 @@ echo "修改wifi名称"
 sed -i "s/OpenWrt/$wifi_name/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 
-echo '添加Adguardhome'
-echo 'CONFIG_PACKAGE_luci-app-adguardhome=y' >> .config
-echo 'CONFIG_PACKAGE_luci-i18n-adguardhome-zh-cn=y'  >> .config 
